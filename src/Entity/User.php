bfa2ae212f883dff74d52f321d64d5af368b07c3
FeryKevin <kevin.fery@outlook.com>
2023-08-19 00:58:59 +0200

    add user entity

---
+++ src/Entity/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Table()]
+class User
+{
+    #[ORM\Column(length: 180, unique: true, nullable: false)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: false)]
+    private ?string $password = null;
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name = null): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
