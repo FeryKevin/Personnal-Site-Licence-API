51a7ac584ca45a649b3c3062d150976d09b098f8
FeryKevin <kevin.fery@outlook.com>
2023-08-19 14:10:15 +0200

    add id

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,12 +8,22 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
     #[ORM\Column(length: 180, unique: true, nullable: false)]
     private ?string $name = null;
 
     #[ORM\Column(length: 255, nullable: false)]
     private ?string $password = null;
 
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    
     public function getName(): ?string
     {
         return $this->name;
